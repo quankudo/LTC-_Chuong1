dd6c9ac657838b5a574a49bce815d8d80871a374
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 23:04:44 +0700

    khởi tạo hàm tìm ucln của 2 số. Complete Bai 7

--- Chuong1_Bai7/Chuong1_Bai7/Program.cs
+++ Chuong1_Bai7/Chuong1_Bai7/Program.cs
@@ -10,6 +10,26 @@ namespace Chuong1_Bai7
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhập số nguyên dương a: ");
+            int a_332 = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhập số nguyên dương b: ");
+            int b_332 = int.Parse(Console.ReadLine());
+
+            int ucln_332 = TimUCLN(a_332, b_332);
+            Console.WriteLine($"Ước chung lớn nhất của {a_332} và {b_332} là: {ucln_332}");
+            Console.ReadKey();
+        }
+
+        public static int TimUCLN(int a_332, int b_332)
+        {
+            while (b_332 != 0)
+            {
+                int temp_332 = b_332;
+                b_332 = a_332 % b_332;
+                a_332 = temp_332;
+            }
+            return a_332;
         }
     }
 }
