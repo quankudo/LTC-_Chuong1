c191dc7ca58d9595b665f680bf3abc06b67ad92c
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 22:51:32 +0700

    khởi tạo hàm tìm chữ số nhỏ nhất. Complete Bai 6

--- Chuong1_Bai6/Chuong1_Bai6/Program.cs
+++ Chuong1_Bai6/Chuong1_Bai6/Program.cs
@@ -10,6 +10,27 @@ namespace Chuong1_Bai6
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhập số nguyên dương n: ");
+            int n_332 = int.Parse(Console.ReadLine());
+
+            int minDigit_332 = TimChuSoNhoNhat(n_332);
+            Console.WriteLine($"Chữ số nhỏ nhất của {n_332} là: {minDigit_332}");
+            Console.ReadKey();
+        }
+
+        public static int TimChuSoNhoNhat(int n_332)
+        {
+            int minDigit_332 = 9; 
+            while (n_332 > 0)
+            {
+                int digit_332 = n_332 % 10;
+                if (digit_332 < minDigit_332)
+                {
+                    minDigit_332 = digit_332; 
+                }
+                n_332 /= 10;
+            }
+            return minDigit_332;
         }
     }
 }
