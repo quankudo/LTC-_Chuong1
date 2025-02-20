f097f5fff679a39acb00910bc8f5e34d43f38525
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 22:34:26 +0700

    Khởi tạo hàm tính tổng ước số

--- Chuong1_Bai3/Chuong1_Bai3/Program.cs
+++ Chuong1_Bai3/Chuong1_Bai3/Program.cs
@@ -10,6 +10,20 @@ namespace Chuong1_Bai3
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        public static int TinhTongUocSo(int n_332)
+        {
+            int sum_332 = 0;
+            for (int i_332 = 1; i_332 <= n_332; i_332++)
+            {
+                if (n_332 % i_332 == 0) // Kiểm tra nếu i_332 là ước số của n_332
+                {
+                    sum_332 += i_332;
+                }
+            }
+            return sum_332;
         }
     }
 }
