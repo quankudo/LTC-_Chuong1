2e4c3b87a8d449b68bd285e9a569fd0c989fcff8
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 23:15:59 +0700

    khởi tạo hàm tính tổng lẽ. Complete Bai 9

--- Chuong1_Bai9/Chuong1_Bai9/Program.cs
+++ Chuong1_Bai9/Chuong1_Bai9/Program.cs
@@ -10,6 +10,22 @@ namespace Chuong1_Bai9
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhập số nguyên dương N: ");
+            int n_332 = int.Parse(Console.ReadLine());
+
+            int sumOdd_332 = TinhTongLe(n_332);
+            Console.WriteLine($"Tổng các số lẻ nguyên dương nhỏ hơn {n_332} là: {sumOdd_332}");
+            Console.ReadKey();
+        }
+
+        public static int TinhTongLe(int n_332)
+        {
+            int sum_332 = 0;
+            for (int i_332 = 1; i_332 < n_332; i_332 += 2) // Duyệt qua các số lẻ
+            {
+                sum_332 += i_332;
+            }
+            return sum_332;
         }
     }
 }
