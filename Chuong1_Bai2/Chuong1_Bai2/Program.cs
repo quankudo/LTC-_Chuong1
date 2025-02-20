362d4adc0a4399b01fc88f0cceba17e9b3afac12
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 22:28:58 +0700

    Khởi tạo hàm liệt kê ước số

--- Chuong1_Bai2/Chuong1_Bai2/Program.cs
+++ Chuong1_Bai2/Chuong1_Bai2/Program.cs
@@ -10,6 +10,18 @@ namespace Chuong1_Bai2
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        public static void LietKeUocSo(int n_332)
+        {
+            for (int i_332 = 1; i_332 <= n_332; i_332++)
+            {
+                if (n_332 % i_332 == 0) // Kiểm tra nếu i_332 là ước số của n_332
+                {
+                    Console.Write(i_332 + " ");
+                }
+            }
         }
     }
 }
