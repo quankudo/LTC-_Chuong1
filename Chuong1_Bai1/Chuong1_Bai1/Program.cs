6c2bd0121cfa6e6e6403e44d38233c54da30c538
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 22:20:23 +0700

    khởi tạo hàm tính tổng s(n)

--- Chuong1_Bai1/Chuong1_Bai1/Program.cs
+++ Chuong1_Bai1/Chuong1_Bai1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace Chuong1_Bai1
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        public static int TinhTong(int n_332)
+        {
+            int sum_332 = 0;
+            for (int i_332 = 1; i_332 <= n_332; i_332++)
+            {
+                sum_332 += i_332;
+            }
+            return sum_332;
         }
     }
 }
