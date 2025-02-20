7840bc47ada8143daeedf22d1e3c61bf99f549e4
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 23:11:28 +0700

    khởi tạo hàm đếm số chữ số. Complete Bai 8

--- Chuong1_Bai8/Chuong1_Bai8/Program.cs
+++ Chuong1_Bai8/Chuong1_Bai8/Program.cs
@@ -10,6 +10,23 @@ namespace Chuong1_Bai8
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhập số nguyên dương n: ");
+            int n_332 = int.Parse(Console.ReadLine());
+
+            int count_332 = DemSoChuSo(n_332);
+            Console.WriteLine($"Số lượng chữ số của {n_332} là: {count_332}");
+            Console.ReadKey();
+        }
+
+        public static int DemSoChuSo(int n_332)
+        {
+            int count_332 = 0;
+            while (n_332 > 0)
+            {
+                count_332++;
+                n_332 /= 10; // Loại bỏ chữ số cuối cùng
+            }
+            return count_332;
         }
     }
 }
