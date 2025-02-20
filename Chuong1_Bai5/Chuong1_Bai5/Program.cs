ff6cc1f7c6e01543df100764c4bb8754fa6fd9c0
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 22:44:40 +0700

    khởi tạo hàm tính tổng chữ số. complete Bai 5

--- Chuong1_Bai5/Chuong1_Bai5/Program.cs
+++ Chuong1_Bai5/Chuong1_Bai5/Program.cs
@@ -10,6 +10,23 @@ namespace Chuong1_Bai5
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhập số nguyên dương n: ");
+            int n_332 = int.Parse(Console.ReadLine());
+
+            int sum_332 = TinhTongChuSo(n_332);
+            Console.WriteLine($"Tổng các chữ số của {n_332} là: {sum_332}");
+            Console.ReadKey();
+        }
+
+        public static int TinhTongChuSo(int n_332)
+        {
+            int sum_332 = 0;
+            while (n_332 > 0)
+            {
+                sum_332 += n_332 % 10; 
+                n_332 /= 10; 
+            }
+            return sum_332;
         }
     }
 }
