b6ffbb1a5dd85a8f00d75f1866a6618623fe2915
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 22:40:12 +0700

    khởi tạo hàm kiểm tra số nguyên tố. complete Bai 4

--- Chuong1_Bai4/Chuong1_Bai4/Program.cs
+++ Chuong1_Bai4/Chuong1_Bai4/Program.cs
@@ -10,6 +10,29 @@ namespace Chuong1_Bai4
     {
         static void Main(string[] args)
         {
+            Console.Write("Nhập số nguyên dương n: ");
+            int n_332 = int.Parse(Console.ReadLine());
+
+            if (KiemTraSoNguyenTo(n_332))
+            {
+                Console.WriteLine($"{n_332} là số nguyên tố.");
+            }
+            else
+            {
+                Console.WriteLine($"{n_332} không phải là số nguyên tố.");
+            }
+            Console.ReadKey();
+        }
+
+        public static bool KiemTraSoNguyenTo(int n_332)
+        {
+            if (n_332 < 2) return false;
+            for (int i_332 = 2; i_332 <= Math.Sqrt(n_332); i_332++) 
+            {
+                if (n_332 % i_332 == 0)
+                    return false; 
+            }
+            return true;
         }
     }
 }
