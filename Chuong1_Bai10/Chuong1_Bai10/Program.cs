0f94fcd8d4520e126fccbad6059eee9dc2d51d4b
quankudo <quannguyen2004aa@gmail.com>
2025-02-20 23:21:38 +0700

    In ra các số nhỏ hơn 100, trừ các số 5, 7, 93. Complete Bai 10

--- Chuong1_Bai10/Chuong1_Bai10/Program.cs
+++ Chuong1_Bai10/Chuong1_Bai10/Program.cs
@@ -10,6 +10,17 @@ namespace Chuong1_Bai10
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Các số lẻ nhỏ hơn 100 (trừ 5, 7, 93):");
+
+            for (int i_332 = 1; i_332 < 100; i_332 += 2) 
+            {
+                if (i_332 == 5 || i_332 == 7 || i_332 == 93)
+                    continue;
+
+                Console.Write(i_332 + " ");
+            }
+            Console.WriteLine(); 
+            Console.ReadKey();
         }
     }
 }
